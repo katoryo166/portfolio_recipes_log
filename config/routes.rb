900aa223d4803074cf2c134aa7fa801b8478a394
katoryo166 <ukiuki.flyingget@gmail.com>
2021-09-10 11:53:58 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+
+  devise_for :admins, :controllers => {
+    :sessions => 'admin/admins/sessions',
+    :registrations => 'admin/admins/registrations',
+    :passwords => 'admin/admins/passwords'
+   }
+
+  devise_for :users, :controller =>{
+    :sessions => 'users/sessions',
+    :registrations => 'users/registrations',
+    :passwords => 'users/passwords'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
