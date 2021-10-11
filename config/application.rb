89490810efaa7a22f059f23b3220595a10baf3ab
katoryo166 <ukiuki.flyingget@gmail.com>
2021-10-11 01:10:36 +0000

    text-color調整

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,7 @@ Bundler.require(*Rails.groups)
 module RacipeLog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-    # config.i18n.load_path += Dir[Rails.root.join('config/locales/ja.yml').to_s]
+    # config.load_defaults 5.2
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
