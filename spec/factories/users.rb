3a1e90ee80aedff51e34772674e9399c875ad63f
katoryo166 <ukiuki.flyingget@gmail.com>
2021-10-12 09:03:04 +0000

    test User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-    factory :user, class: :user do
+  factory :user do
     name { Faker::Name.name }
     sequence(:email) { |n| "example#{n}@example.com" }
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')) }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,47 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'バリデーションのテスト' do
-    subject { user.valid? }
-
-    let!(:other_user) { create(:user) }
-    let(:user) { build(:user) }
-
-    context 'nameカラム' do
-      it '空欄でないこと' do
-        user.name = ''
-        is_expected.to eq false
-      end
-      it '2文字以上であること: 1文字は×' do
-        user.name = Faker::Lorem.characters(number: 1)
-        is_expected.to eq false
-      end
-      it '2文字以上であること: 2文字は〇' do
-        user.name = Faker::Lorem.characters(number: 2)
-        is_expected.to eq true
-      end
-      it '20文字以下であること: 20文字は〇' do
-        user.name = Faker::Lorem.characters(number: 20)
-        is_expected.to eq true
-      end
-      it '20文字以下であること: 21文字は×' do
-        user.name = Faker::Lorem.characters(number: 21)
-        is_expected.to eq false
-      end
-      it '一意性があること' do
-        user.name = other_user.name
-        is_expected.to eq false
+  describe '#User' do
+    context '名前、メール、パスワードがあるとき' do
+      it "有効である" do
+        user = User.new(
+        name: "tarou",
+         email: "testman@example.com",
+         password: "password",
+         )
+         # オブジェクトをexpectに渡した時に、有効である(be valid)という意味になります
+         expect(user).to be_valid
       end
     end
-
-    context 'introductionカラム' do
-      it '50文字以下であること: 50文字は〇' do
-        user.introduction = Faker::Lorem.characters(number: 50)
-        is_expected.to eq true
-      end
-      it '50文字以下であること: 51文字は×' do
-        user.introduction = Faker::Lorem.characters(number: 51)
-        is_expected.to eq false
+    context '名前がないとき' do
+      it "無効である" do
+        user = User.new(
+         name: nil,
+         email: "testman@example.com",
+         password: "password"
+         )
+         user.valid?
+         expect(user.errors[:name]).to include("が入力されていません。")
       end
     end
   end
