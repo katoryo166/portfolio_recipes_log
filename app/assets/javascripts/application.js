4495e1870f0ebbd9835f5dd26d151f836f838247
katoryo166 <ukiuki.flyingget@gmail.com>
2021-11-17 23:02:55 +0000

    font変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,22 +19,4 @@
 //= require turbolinks
 //= require_tree .
 
-$("turbolinks:load",function(){
-  var pagetop = $('#page_top');
-  // ボタン非表示
-  pagetop.hide();
-
-  // 100px スクロールしたらボタン表示
-  $(window).scroll(function () {
-     if ($(this).scrollTop() > 200) {
-          pagetop.fadeIn();
-     } else {
-          pagetop.fadeOut();
-     }
-  });
-  pagetop.click(function () {
-     $('body, html').animate({ scrollTop: 0 }, 500);
-     return false;
-  });
-});
 
