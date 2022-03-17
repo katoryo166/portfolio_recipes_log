3aa710722e72abeec34374e456df9434c636720f
katoryo166 <ukiuki.flyingget@gmail.com>
2022-03-17 00:09:24 +0000

    smoothsscroll記述場所

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,27 @@
 //= require cocoon
 /*global $*/
 
+/*スムーススクロール*/
+$("turbolinks:load",function(){
+        var pagetop = $('#page_top');
+        // ボタン非表示
+        pagetop.hide();
+
+        // 100px スクロールしたらボタン表示
+        $(window).scroll(function () {
+           if ($(this).scrollTop() > 200) {
+                pagetop.fadeIn();
+           } else {
+                pagetop.fadeOut();
+           }
+        });
+        pagetop.click(function () {
+           $('body, html').animate({ scrollTop: 0 }, 500);
+           return false;
+        });
+      });
+
+/*about slick*/
 $("turbolinks:load", function() {
     $('.slide').slick({
        dots: true,
